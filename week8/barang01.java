b0ae25a5b5a324200c43888f5ab761a635502cc4
Agilabrs <arbiansyah.agil11@gmail.com>
2024-04-13 09:50:30 +0700

    boleh

--- week8/barang01.java
+++ week8/barang01.java
@@ -5,8 +5,8 @@ public class barang01 {
     String nama, kategori, barangBaru;
 
     barang01(int kode, String nama, String kategori) {
-        kode = this.kode;
-        nama = this.nama;
-        kategori = this.kategori;
+        this.kode = kode;
+        this.nama = nama;
+        this.kategori = kategori;
     }
 }
